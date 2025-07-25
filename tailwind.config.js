ef2aec253be5bc6199c9773e76fe19424e8a8528
hooney1218 <62353845+hooney1218@users.noreply.github.com>
2025-07-25 17:00:09 +0900

    🚀 배포: 2025-07-25

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       animation: {
@@ -12,4 +18,5 @@ module.exports = {
       },
     },
   },
+  plugins: [],
 } 
